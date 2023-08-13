93e55e4c614a000d1110ce777ac2f27854b253a5
Sansss8 <hasan080801@gmail.com>
2023-08-13 20:40:29 +0700

    inisialisasi project sistem pakar

---
+++ login.php
@@ -0,0 +1,55 @@
+ <title>Login Gagal</title>
+<?php
+session_start();
+include "config/koneksi.php";
+
+$user=$_POST['username'];
+$pass=md5($_POST['password']);
+
+$login=mysqli_query($conn,"select * from admin where username='$user' and password='$pass'");
+
+$ketemu=mysqli_num_rows($login);
+$r=mysqli_fetch_array($login);
+if ($ketemu>0) {
+	$_SESSION['username'] = $r['username'];
+	$_SESSION['password'] = $r['password'];
+	$_SESSION['nama_lengkap'] = $r['nama_lengkap'];
+	header("location: index.php");
+}
+else{
+  echo " <link href='css/font-awesome-4.2.0/font-awesome-4.2.0/css/font-awesome.min.css' rel='stylesheet'>
+	
+		<link href='css/main.css' rel='stylesheet' type='text/css' media='all'/>
+		    <link rel='stylesheet' href='aset/bootstrap.css'>
+		<div class='errorpage'> <center><div class='danger'><i class='fa fa-exclamation-triangle'></i></div><br><h1>Teutiasa Lebet!</h1>
+       Hapunteun Username sinareng Password na lepat.<br><br><input name='submit' id='submitku' type=submit style='padding: 6px 12px;' value='ULANGI LAGI' onclick=location.href='formlogin'></a><br><br><a href='formlogin'>Halaman login</a></p></center></div>
+<div class='chick-wrapper-landing show'>
+  <div class='wing-back'></div>
+  <div class='body'>
+    <div class='eye-left'></div>
+    <div class='eye-right'></div>
+  </div>
+  <div class='wing-front'></div>
+</div>
+<div class='chick-wrapper-run run'><img class='egg-lay' src='animasi/login/1.png'/>
+  <div class='legs'>
+    <div class='leg-l'></div>
+    <div class='leg-r'></div>
+  </div>
+  <div class='wing-back'> </div>
+  <div class='sweat-1'></div>
+  <div class='sweat-2'></div>
+  <div class='sweat-3'></div>
+  <div class='body'>
+    <div class='eye-liner'>
+      <div class='eye'></div>
+    </div>
+    <div class='eye-lid'></div>
+    <div class='cheek'></div>
+  </div>
+  <div class='sweat-last'></div>
+  <div class='wing-front'></div>
+</div>
+    <script  src='animasi/login/index.js'></script>";
+}
+?>
